68ba4ece555c362c5fa2f7b45cb72affd0b866ef
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-04-29 20:14:18 +0530

    hee

--- my-project/pages/Button.js
+++ my-project/pages/Button.js
@@ -5,31 +5,31 @@ const Button = () => {
     <div>
       <button
         type="button"
-        class="text-white  bg-purple-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 "
+        className="text-white  bg-purple-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 "
       >
         All Post
       </button>
       <button
         type="button"
-        class="text-white  bg-purple-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 "
+        className="text-white  bg-purple-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 "
       >
        Text
       </button>
       <button
         type="button"
-        class="text-white  bg-purple-600  focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 "
+        className="text-white  bg-purple-600  focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 "
       >
        Video
       </button>
       <button
         type="button"
-        class="text-white  bg-purple-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 "
+        className="text-white  bg-purple-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 "
       >
         Audio
       </button>
       <button
         type="button"
-        class="text-white  bg-purple-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 "
+        className="text-white  bg-purple-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center m-2 "
       >
         Images
       </button>
